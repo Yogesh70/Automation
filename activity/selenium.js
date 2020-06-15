d9065bbe4aa24a2dd08d5fc25d813b50ae1584fc
Yogesh Sharma <yogesh.rohtak2000@gmail.com>
2020-06-15 12:26:13 +0530

    Promise

--- activity/selenium.js
+++ activity/selenium.js
@@ -7,8 +7,8 @@ let browser = new swd.Builder();
 //tabs=> tab
 let tab = browser.forBrowser("chrome").build();
 let tabWillBeOpenedPromise = tab.get("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
+let gcodesElements, gcInputBox, gTextArea;
 tabWillBeOpenedPromise
-    
     .then(function()  {
         //implicit timeout
         let findTimeOutP = tab.manage().setTimeouts({
@@ -117,15 +117,158 @@ tabWillBeOpenedPromise
         return new Promise(function(resolve, reject) {
             //logic to solve a question
             let allCBTnWSP = tab.findElements(swd.By.css(".challenge-submit-btn"));
-            allCBTnWSP.then(function(cBtnArr) {
+        allCBTnWSP
+            .then(function(cBtnArr) {
                 let cBtnWilBeClickedP = cBtnArr[0].click();
                 return cBtnWilBeClickedP;  
+            })
+            .then(function() {
+                let editorialWillBeSelectedPromise = tab.findElement(swd.By.css("a[data-attr2='Editorial']"));
+                return editorialWillBeSelectedPromise;
+            })
+            .then(function(editorBtn) {
+                let editorBtnWillBeclickedP = editorBtn.click();
+                return editorBtnWillBeclickedP;
+            })
+            .then(function() {
+                let hlBtnP = handleLockBtn();
+                console.log(hlBtnP)
+                return hlBtnP;
+            })
+            .then(function() {
+                let cCodeWillBecopied = copyCode();
+                return cCodeWillBecopied;
+            })
+            .then(function(code) {
+                let codeWillBePastedP = pasteCode(code);
+                return codeWillBePastedP;
             })
             .then(function() {
                 resolve();
             })
             .catch(function(err) {
-                reject();
+                reject(err);
             })
         })
     }
+
+    function handleLockBtn() {
+        return new Promise(function(resolve,reject) {
+           let lockBtnWillBeFP = tab.findElement(swd.By.css(".editorial-content-locked button.ui-btn.ui-btn-normal"));
+        lockBtnWillBeFP
+           .then(function(lockBtn) {
+                let lockBtnWillBeCP = lockBtn.click();
+                return lockBtnWillBeCP;
+           })
+           .then(function() {
+                resolve();
+           })
+           .catch(function() {
+               console.log("Lock button wasn't found"); 
+               resolve();
+           })
+        })
+    }
+
+    function copyCode() {
+        return new Promise(function(resolve, reject) {
+            //get all the lang array
+            let allLangElementP = tab.findElements(swd.By.css(".hackdown-content h3"));
+            //get all the code array
+            let allcodeElementP = tab.findElements(swd.By.css(".hackdown-content .highlight"));
+            let bothArrayP = Promise.all([allLangElementP, allcodeElementP]);
+        bothArrayP
+            .then(function(bothArrays) {
+                let langsElements = bothArrays[0];
+                gcodesElements = bothArrays[1];
+                let alllangTextP = [];
+                for(let i= 0; i< langsElements.length; i++) {
+                  let cLangsP = langsElements[i].getText();
+                    alllangTextP.push(cLangsP);
+                }   
+            return Promise.all(alllangTextP);
+        }).then(function(allLang) {
+            let codeOfCP;
+            for(let i =0; i< allLang.length;i++) {
+                if(allLang[i].includes("C++")) {
+                   codeOfCP = gcodesElements[i].getText();
+                   break;
+                }
+            }
+            return codeOfCP;
+        }).then(function(code) {
+            console.log(code);
+            resolve(code);
+        }).catch(function(err) {
+            reject(err);
+    })
+        })
+    }
+
+    function pasteCode(code) {
+        return new Promise(function(resolve, reject) {
+            //click on Problem tab
+            let pTabWillBeSelected = tab.findElement(swd.By.css("li#Problem"));
+            pTabWillBeSelected
+                .then(function(pTab) {
+                    let pTabWillBeClicked = pTab.click();
+                    return pTabWillBeClicked;
+                })
+                .then(function() {
+                    let testInputWillBeFP = tab.findElement(swd.By.css(".custom-checkbox.inline"));
+                    return testInputWillBeFP;
+                })
+                .then(function(testInput) {
+                    let testInputWillBeCP = testInput.click();
+                    return testInputWillBeCP;
+                })
+                .then(function() {
+                    let customInputAreaWillBeSP = tab.findElement(swd.By.css(".custominput"));
+                    return customInputAreaWillBeSP;
+                })
+                .then(function(cArea) {
+                    gcInputBox = cArea;
+                    let codeWillBeEP = cArea.sendKeys(code);
+                    return codeWillBeEP;
+                })
+                .then(function() {
+                    let ctrlAWillBeSendP = gcInputBox.sendKeys(swd.Key.CONTROL + "a");
+                    return ctrlAWillBeSendP;  
+                })
+                .then(function() {
+                    let ctrlXWillBeSendP = gcInputBox.sendKeys(swd.Key.CONTROL + "x");
+                    return ctrlXWillBeSendP;
+                })
+                .then(function() {
+                    let tAreaP = tab.findElement(swd.By.css("textarea"));
+                    return tAreaP;
+                })
+                .then(function(tArea) {
+                    gTextArea = tArea;
+                    let codeWillBeEP = tArea.sendKeys(swd.Key.CONTROL + "a")
+                    return codeWillBeEP;
+                })
+                .then(function() {
+                    let ctrlVWillBeSendP = gTextArea.sendKeys(swd.Key.CONTROL + "v");
+                    return ctrlVWillBeSendP;
+                })
+                .then(function() {
+                    let submitCodeBtnWillBeSP = tab.findElement(swd.By.css(".hr-monaco-submit"));
+                    return submitCodeBtnWillBeSP;
+                })
+                .then(function(submitBtn) {
+                    let submitBtnWillBeCP = submitBtn.click();
+                    return submitBtnWillBeCP;
+                })
+                .then(function() {
+                    resolve();
+                })
+                .catch(function(err) {
+                    reject(err);
+                })
+            //write the code
+            //submit the code
+        })       
+    }
+
+    
